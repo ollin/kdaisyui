d2be9c3afddff8cca109504e4b92ef8da417cb39
Oliver Nautsch <oliver.nautsch@gmail.com>
2026-03-29 15:18:52 +0200

    feat(release): publish lib to GitHub Packages with semantic versioning
    - Add maven-publish plugin to lib with artifactId 'kdaisyui'
    - Configure GitHub Packages as publish repository
    - Add release.yml workflow: publishes on push to main
    - Version: 1.0.{github.run_number} (patch = build number)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("kdaisyui.kotlin-library-conventions")
+    `maven-publish`
 }
+
+version = project.findProperty("version")?.toString() ?: "0.0.1-SNAPSHOT"
+group = "io.github.ollin"
 
 repositories {
     mavenCentral()
@@ -14,6 +18,25 @@ testing {
     suites {
         val test by getting(JvmTestSuite::class) {
             useKotlinTest(project.property("versions.kotlin").toString())
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "kdaisyui"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ollin/kdaisyUI")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }

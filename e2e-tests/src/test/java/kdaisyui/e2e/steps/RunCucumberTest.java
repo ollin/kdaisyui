237a7f3cd2646f7695303fe5e6576fee967797ca
Oliver Nautsch <oliver.nautsch@gmail.com>
2026-05-20 17:16:34 +0200

    fix(e2e-tests): replace deprecated classpath resource selector with package selector
    Cucumber warns that @SelectClasspathResource with slash-separated paths
    should not be used to select features in a package. Use @SelectPackages
    with dot-separated package name instead.

--- e2e-tests/src/test/java/kdaisyui/e2e/steps/RunCucumberTest.java
+++ e2e-tests/src/test/java/kdaisyui/e2e/steps/RunCucumberTest.java
@@ -2,7 +2,7 @@ package kdaisyui.e2e.steps;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("kdaisyui/e2e/steps")
+@SelectPackages("kdaisyui.e2e.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:build/reports/cucumber/cucumber-report.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "kdaisyui.e2e.steps")
 public class RunCucumberTest {
